8f187f6d934cfc00fb1b5f7d0becfa2abc7fb187
Zandile Gebuza <2562617@students.wits.ac.za>
2023-09-17 14:38:33 +0200

    UI Menu

---
+++ Assignment4/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+  //This activates the "Play"button's functionality
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+  //The game will end when the "Quit" button is pressed
+    public void Quit()
+    { 
+        Application.Quit();
+    } 
+}
