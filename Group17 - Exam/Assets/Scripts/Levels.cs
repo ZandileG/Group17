28ace9ca9a02d62c9aa5403c58c1bdf0325a19b8
Mpumiii <130597279+Mpumiii@users.noreply.github.com>
2023-10-19 09:58:00 +0200

    Add files via upload
    Blockout

---
+++ Group17 - Exam/Assets/Scripts/Levels.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Levels : MonoBehaviour
+{
+    public GameObject level2;
+
+    //public void StartLevel()
+}
